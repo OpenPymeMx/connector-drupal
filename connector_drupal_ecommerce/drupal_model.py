46d872c326a2c94cce52a8577ba14378d6ee872d
agb80 <atin81@gmail.com>
2016-06-30 13:50:28 -0500

    refactor(drupal_module): Create an extra funcion that allow properly add more backends with out need

--- connector_drupal_ecommerce/drupal_model.py
+++ connector_drupal_ecommerce/drupal_model.py
@@ -23,11 +23,6 @@
 
 from openerp.osv import orm, fields
 from openerp.tools.translate import _
-from openerp.tools import SUPERUSER_ID
-
-from openerp.addons.connector.session import ConnectorSession
-
-from .unit.export_synchronizer import export_record
 
 
 class DrupalBackend(orm.Model):
@@ -39,13 +34,20 @@ class DrupalBackend(orm.Model):
     _inherit = 'connector.backend'
     _backend_type = 'drupal'
 
-    def _select_versions(self, cr, uid, context=None):
+    def select_versions(self, cr, uid, context=None):
         """ Available versions in the backend.
         Can be inherited to add custom versions.  Using this method
         to add a version from an ``_inherit`` does not constrain
         to redefine the ``version`` field in the ``_inherit`` model.
         """
         return [('7', '7')]
+
+    def _select_versions(self, cr, uid, context=None):
+        """ Available versions in the backend.
+        If you want to add a version, do not override this
+        method, but ``select_version``.
+        """
+        return self.select_versions(cr, uid, context=context)
 
     def _select_timezones(self, cr, uid, context=None):
         """ List all the available timezones"""
